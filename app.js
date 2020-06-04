b5b4d3480b00536e1d0ee34a2af1d0ce7544a82f
pullip lee <pullip.lee1023@gmail.com>
2020-06-04 14:30:49 +0800

    handlebar added

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var pg = require('pg');
+const express = require('express');
+const app = express();
+const hb = require('express-handlebars');
+
 
 const knex = require('knex')({
     client: 'postgresql',
@@ -12,3 +16,27 @@ const knex = require('knex')({
 var client = new pg.Client('postgres://xccelerate:password@localhost:5432/bookings');
 
 client.connect();
+
+//connecting handlebars
+app.engine('handlebars', hb({ defaultLayout: 'main' }));
+    app.set('view engine', 'handlebars'); 
+
+    app.use(express.static("public"));
+
+    app.get('/', (req, res)=>{
+        res.render('index' )
+    });
+
+    app.get('/recipes', (req, res)=>{
+        res.render('recipe' )
+    });
+
+    app.get('/search', (req, res)=>{
+        res.render('search');
+    });
+
+
+    app.get('/mypage', (req, res)=>{
+        res.render('mypage' )
+    });
+
