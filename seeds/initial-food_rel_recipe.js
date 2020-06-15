56538d0f4b8ce8060db82f76011a65583bcdc50c
Kam Yiu Cheung <tyt11123@yahoo.com.hk>
2020-06-15 12:50:24 +0800

    migrations and seeds amendment

--- seeds/initial-food_rel_recipe.js
+++ seeds/initial-food_rel_recipe.js
@@ -11,7 +11,7 @@ exports.seed = function(knex) {
         {id: 4, food_id: 5, recipe_id: 4},
         {id: 5, food_id: 7, recipe_id: 5},
         {id: 6, food_id: 10, recipe_id: 6},
-        {id: 7, food_id: 15, recipe_id: 7}
+        {id: 7, food_id: 1, recipe_id: 7}
       ]);
     });
 };
