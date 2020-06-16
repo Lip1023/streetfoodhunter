912f0bd0233ddfdb06af637d0f2824888d663bc4
Kam Yiu Cheung <tyt11123@yahoo.com.hk>
2020-06-16 11:17:45 +0800

    Just login

--- passport/strategies/local-strategy.js
+++ passport/strategies/local-strategy.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('./bcrypt')
+const bcrypt = require('./bcrypt');
 const knex = require('knex')({
     client: 'postgresql',
     connection: {
@@ -38,6 +38,7 @@ passport.use('local-signup', new LocalStrategy({
       },
     async (req, email, password, done) => {
         try {
+            console.log(req.body.sa);
             let index = email.indexOf("@");
             let user_name = email.slice(0, index);
             await knex.raw('SELECT setval(\'"userTable_id_seq"\', (SELECT MAX(id) from "userTable"));');
@@ -46,12 +47,13 @@ passport.use('local-signup', new LocalStrategy({
                 return done (null, false, {message: "Email Already Taken"});
             }
             let hash = await bcrypt.hashPassword(password)
+            let hash2 = await bcrypt.hashPassword(req.body.sa)
             const newUser = {
                 full_name: user_name,
                 user_name: user_name,
                 email: email,
                 password: hash,
-                security_answer: req.body.sa
+                security_answer: hash2
             };
             let userId = await knex('userTable').insert(newUser). returning('id');
             newUser.id = userId[0];
@@ -60,6 +62,34 @@ passport.use('local-signup', new LocalStrategy({
             done(err)
         }
     }
+));
+
+passport.use('local-change', new LocalStrategy({
+    passReqToCallback: true
+  },
+async (req, email, password, done) => {
+    try {
+        let users = await knex('userTable').where({email:email});
+        if(users.length ==0){
+            return done(null, false, {message: 'Incorrect Credentials.'})
+        } 
+        let user = users[0];
+        let result = await bcrypt.checkPassword(password, user.security_answer);
+        if(result){
+            let hash = await bcrypt.hashPassword(password);
+            user.password = hash;
+            let id = user.id;
+            delete user.id;
+            await knex('userTable').where("id",id).update(user);
+            user.id = id;
+            return done(null, user);
+        } else{
+            return done(null, false, {message: 'Incorrect Credentials.'})
+        }
+    }catch(err){
+        done(err)
+    }
+}
 ))
 
 
@@ -68,7 +98,7 @@ passport.use('local-signup', new LocalStrategy({
     });
 
     passport.deserializeUser(async(id,done)=>{
-        let users = await knex('users').where({id:id});
+        let users = await knex('userTable').where({id:id});
         if(users.length==0){
             return done(new Error(`Wrong User id ${id}`))
         } 
--- seeds/initial-user.js
+++ seeds/initial-user.js
@@ -5,11 +5,11 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('userTable').insert([
-        {id: 1, full_name: 'John',user_name: 'super_john', email:'superjohn@abc.com', password:'$2b$10$3cD3ALX2vbvgdYqlD8etuOpMZT4yrM1Zu1oM5QIVaEFL7y25RyNhG', security_answer:'Hong Kong'},
-        {id: 2, full_name: 'Mary',user_name: 'thisismary', email:'thisismary@abc.com', password:'$2b$10$3cD3ALX2vbvgdYqlD8etuOpMZT4yrM1Zu1oM5QIVaEFL7y25RyNhG', security_answer:'Hong Kong'},
-        {id: 3, full_name: 'Smith',user_name: 'samsmith', email:'samsmith@abc.com', password:'$2b$10$3cD3ALX2vbvgdYqlD8etuOpMZT4yrM1Zu1oM5QIVaEFL7y25RyNhG', security_answer:'Hong Kong'},
-        {id: 4, full_name: 'Stark',user_name: 'Tony', email:'tonystrak@abc.com', password:'$2b$10$3cD3ALX2vbvgdYqlD8etuOpMZT4yrM1Zu1oM5QIVaEFL7y25RyNhG', security_answer:'Hong Kong'},
-        {id: 5, full_name: 'Bane',user_name: 'villainbane', email:'villainebane@abc.com', password:'$2b$10$3cD3ALX2vbvgdYqlD8etuOpMZT4yrM1Zu1oM5QIVaEFL7y25RyNhG', security_answer:'Hong Kong'},
+        {id: 1, full_name: 'John',user_name: 'super_john', email:'superjohn@abc.com', password:'$2b$10$3cD3ALX2vbvgdYqlD8etuOpMZT4yrM1Zu1oM5QIVaEFL7y25RyNhG', security_answer:'$2b$10$bZfVcwgvChclwISCrU6iIOUzIXudhPsgNVlfgwo7o4Ze2Uk4NqUqW'},
+        {id: 2, full_name: 'Mary',user_name: 'thisismary', email:'thisismary@abc.com', password:'$2b$10$3cD3ALX2vbvgdYqlD8etuOpMZT4yrM1Zu1oM5QIVaEFL7y25RyNhG', security_answer:'$2b$10$bZfVcwgvChclwISCrU6iIOUzIXudhPsgNVlfgwo7o4Ze2Uk4NqUqW'},
+        {id: 3, full_name: 'Smith',user_name: 'samsmith', email:'samsmith@abc.com', password:'$2b$10$3cD3ALX2vbvgdYqlD8etuOpMZT4yrM1Zu1oM5QIVaEFL7y25RyNhG', security_answer:'$2b$10$bZfVcwgvChclwISCrU6iIOUzIXudhPsgNVlfgwo7o4Ze2Uk4NqUqW'},
+        {id: 4, full_name: 'Stark',user_name: 'Tony', email:'tonystrak@abc.com', password:'$2b$10$3cD3ALX2vbvgdYqlD8etuOpMZT4yrM1Zu1oM5QIVaEFL7y25RyNhG', security_answer:'$2b$10$bZfVcwgvChclwISCrU6iIOUzIXudhPsgNVlfgwo7o4Ze2Uk4NqUqW'},
+        {id: 5, full_name: 'Bane',user_name: 'villainbane', email:'villainebane@abc.com', password:'$2b$10$3cD3ALX2vbvgdYqlD8etuOpMZT4yrM1Zu1oM5QIVaEFL7y25RyNhG', security_answer:'$2b$10$bZfVcwgvChclwISCrU6iIOUzIXudhPsgNVlfgwo7o4Ze2Uk4NqUqW'},
       ]);
     });
 };
